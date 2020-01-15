996c564b135e3deee6c966dec0a98412503ae04f
Horváth Gellért <gellerthorvath0@gmail.com>
2020-01-15 12:54:19 +0100

    Kezedett

---
+++ FormBills.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TobbbformosPizzaAlkalmazasTobbTabla.Model;
+using TobbbformosPizzaAlkalmazasTobbTabla.Repository;
+
+namespace TobbformosMvcPizzaTobbTabla
+{
+    public partial class FormPizzaFutarKft : Form
+        
+    {
+        private bool nincsenekListViewOszlopok= true;
+
+
+        private void tabPageSzamlak_Click(object sender, EventArgs e)
+        {
+            beallitSzamlakTabPagetIndulaskor();
+
+        }
+        private void tabControlPizzaFutarKFT_Selected(object sender, TabControlEventArgs e)
+        {
+            beallitSzamlakTabPagetIndulaskor();
+            feltoltComboBoxotMegrendelokkel();
+        }
+
+        private void feltoltComboBoxotMegrendelokkel()
+        {
+            comboBoxMegrendelok.DataSource = repo.getCustomersName();
+        }
+
+        private void beallitSzamlakTabPagetIndulaskor()
+        {
+            
+            listViewRendelesek.GridLines = true;
+            listViewRendelesek.View = View.Details;
+            listViewRendelesek.Columns.Add("Azonosító");
+            listViewRendelesek.Columns.Add("Futár");
+            listViewRendelesek.Columns.Add("Megrendelő");
+            listViewRendelesek.Columns.Add("Dátum");
+            listViewRendelesek.Columns.Add("idő");
+            listViewRendelesek.Columns.Add("Teljesités");
+            nincsenekListViewOszlopok = false;
+            listViewRendelesek.Visible = false;
+            labelRendelesek.Visible = false;
+            dataGridViewTetelek.Visible = false;
+            labelTetelek.Visible = false;
+
+        }
+        private void comboBoxMegrendelok_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBoxMegrendelok.SelectedIndex < 0)
+            {
+                return;
+            }
+            listViewRendelesek.Visible = true;
+
+            string megrendeloNev = comboBoxMegrendelok.Text;
+            feltoltListViewAdatokkal(megrendeloNev);
+
+        }
+
+        private void feltoltListViewAdatokkal(string megrendeloNev)
+        {
+            List<Order> megrendelesek=repo.getOrders(megrendeloNev);
+            listViewRendelesek.Items.Clear();
+
+            foreach (Order megrendeles in megrendelesek)
+            {
+                ListViewItem lvi = new ListViewItem(megrendeles.getOrderId().ToString());
+                lvi.SubItems.Add(megrendeles.getCourierId().ToString());
+                lvi.SubItems.Add(megrendeles.getCustomerId().ToString());
+                lvi.SubItems.Add(megrendeles.getDate().ToString());
+                lvi.SubItems.Add(megrendeles.getTime().ToString());
+                lvi.SubItems.Add(megrendeles.getDone().ToString());
+
+                listViewRendelesek.Items.Add(lvi);
+
+            }
+        }
+    }
+}
---
+++ repository/Order/RepositoryDatabaseTableOrderTestData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TobbbformosPizzaAlkalmazasTobbTabla.Model;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace TobbbformosPizzaAlkalmazasTobbTabla.Repository
+{
+    partial class RepositoryDatabaseTableOrder
+    {
+   
+        public void fillOrderWithTestDataFromSQLCommand()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+
+                string query =
+                    "INSERT INTO `prendeles` (`razon`, `vazon`, `fazon`, `datum`, `ido`,`teljesites`) VALUES " +
+                            "(1, 4, 2, '2010-10-01', 13.15,1), " +
+                            "(2, 7, 2, '2010-10-01', 14.17,0), " +
+                            "(3, 1, 1, '2010-10-02', 11.07,0), " +
+                            "(4, 5, 2, '2010-10-02', 14.55,1), " +
+                            "(5, 2, 3, '2010-10-02', 15.27,1), " +
+                            "(6, 4, 2, '2010-10-03', 15.58,1), " +
+                            "(7, 6, 2, '2010-10-04', 11.44,1), " +
+                            "(8, 7, 4, '2010-10-04', 12.11,1), " +
+                            "(9, 1, 5, '2010-10-04', 14.33,1), " +
+                            "(10, 3, 5, '2010-10-04', 18.04,1), " +
+                            "(11, 2, 1, '2010-10-05', 16.38,1), " +
+                            "(12, 5, 2, '2010-10-05', 17.02,1), " +
+                            "(13, 6, 2, '2010-10-06', 12.17,1), " +
+                            "(14, 4, 3, '2010-10-06', 13.21,1), " +
+                            "(15, 1, 4, '2010-10-06', 15.05,1), " +
+                            "(16, 2, 5, '2010-10-06', 15.59,1), " +
+                            "(17, 7, 2, '2010-10-06', 18.44,1), " +
+                            "(18, 3, 2, '2010-10-07', 12.01,1), " +
+                            "(19, 4, 5, '2010-10-07', 13.44,1), " +
+                            "(20, 1, 1, '2010-10-07', 17.25,1), " +
+                            "(21, 5, 3, '2010-10-08', 14.29,1); ";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+                Debug.WriteLine(e.Message);
+                throw new RepositoryException("Tesztadatok feltöltése sikertelen.");
+            }
+        }
+    }
+}
