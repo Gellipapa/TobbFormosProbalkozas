40917dcc7eac0c8acc0d248a11cd42c0edd67842
Horváth Gellért <gellerthorvath0@gmail.com>
2020-01-22 13:12:21 +0100

    FinalPrice megírása

--- repository/OrderItemsView/RepositoryOrderItemsView.cs
+++ repository/OrderItemsView/RepositoryOrderItemsView.cs
@@ -12,15 +12,22 @@ namespace TobbbformosPizzaAlkalmazasTobbTabla.repository.OrderItemsView
     class RepositoryOrderItemsView
     {
         private List<OrderItemsView2> roiv;
+        /// <summary>
+        /// Fizetendő végösszeg
+        /// </summary>
+        private int finalPrice=0; 
 
 
         public RepositoryOrderItemsView(int orderNumber, List<Item>items, List<Pizza>pizzas)
         {
 
             List<Item> iviews = items.FindAll(i => i.getOrderId() == orderNumber);
+
             foreach(Item i in iviews)
             {
                 Pizza pizza = pizzas.Find(p => p.getId() == i.getPizzaId());
+                finalPrice = 0;
+                finalPrice = finalPrice + i.getPiece() * pizza.getPrice();
                 OrderItemsView2 oiv = new OrderItemsView2(
                     orderNumber,
                     i.getPiece(),
@@ -31,6 +38,11 @@ namespace TobbbformosPizzaAlkalmazasTobbTabla.repository.OrderItemsView
                 roiv.Add(oiv);
             }
 
+        }
+
+        public int getFinalPrice()
+        {
+            return finalPrice;
         }
 
     }
